eb99e51e06b8148e18eb05f2e7ee3ebddcffb235
datomezz <datomezz@gmail.com>
2024-05-30 11:48:56 +0400

    init

---
+++ dist/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const app = express();
+const PORT = 3000;
+const VARIABLES = {
+    URL: "https://rakhdeba.com/",
+    SELECTOR: ".posts > article",
+    FIELDS: {
+        LABEL: "div > a.source",
+        TITLE: "p",
+        URL: "div > a",
+    },
+};
+app.get("/", (req, res) => {
+    res.send("Beglar API");
+});
+app.get("/parse", async (req, res) => {
+    try {
+        const { data } = await axios.get(VARIABLES.URL);
+        const $ = cheerio.load(data);
+        const scrapedData = [];
+        $(VARIABLES.SELECTOR).each((_, element) => {
+            const item = {
+                label: $(element).find(VARIABLES.FIELDS.LABEL).text(),
+                title: $(element).find(VARIABLES.FIELDS.TITLE).text(),
+                url: $(element).find(VARIABLES.FIELDS.URL).eq(0).attr("href"),
+            };
+            scrapedData.push(item);
+        });
+        res.json(scrapedData);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send('Parser Error - 500');
+    }
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
